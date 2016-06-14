8c266b6f2cb6da6d79515529bf22a838e503c981
David Moreau-Simard <dms@redhat.com>
2016-06-14 15:00:06 -0400

    Explicitely installing erlang before rabbitmq is no longer necessary
    And can actually cause dependency issues as per
    https://bugzilla.redhat.com/show_bug.cgi?id=1346240

--- puppet/modules/centos_cloud/manifests/controller/rabbitmq.pp
+++ puppet/modules/centos_cloud/manifests/controller/rabbitmq.pp
@@ -1,10 +1,4 @@
 class centos_cloud::controller::rabbitmq {
-
-  package { 'erlang':
-    ensure => 'installed',
-    before => Class['::rabbitmq']
-  }
-
   class { '::rabbitmq':
     delete_guest_user => true,
     repos_ensure      => false,
