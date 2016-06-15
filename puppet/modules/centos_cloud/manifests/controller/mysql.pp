08878a626c780c36cb2fb574ba04e42f0f60e98b
David Moreau-Simard <dms@redhat.com>
2016-06-15 11:58:19 -0400

    Ensure a matching ulimit is set for mysql max connections
    MariaDB won't take into account a non-default max_connections
    configuration if it doesn't have enough "max open files".

--- puppet/modules/centos_cloud/manifests/controller/mysql.pp
+++ puppet/modules/centos_cloud/manifests/controller/mysql.pp
@@ -1,4 +1,19 @@
 class centos_cloud::controller::mysql {
+
+  file { '/etc/systemd/system/mariadb.service.d/':
+    ensure => directory
+  }->
+  file { '/etc/systemd/system/mariadb.service.d/limits.conf':
+    ensure => present,
+    source => "puppet:///modules/${module_name}/limits.conf",
+    notify => [ Exec['Reload systemctl'], Service['mysqld'] ]
+  }
+
+  exec { 'Reload sytemctl':
+    command     => "/usr/bin/systemctl daemon-reload",
+    refreshonly => true
+  }
+
   class { '::mysql::server':
     override_options => {
       'mysqld' => {
