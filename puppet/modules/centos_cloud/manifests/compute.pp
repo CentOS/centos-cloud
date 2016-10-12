e0a66e6d33d05657f42cbefd81d5d02c675f42a2
David Moreau-Simard <dms@redhat.com>
2016-10-12 17:28:24 -0400

    Manage kernel modules with puppet-kmod instead of "manually"
    puppet-kmod will take care of reloading the module when the
    initial configuration is done.
    For example, the previous way would configure kvm_intel nested to
    be enabled but would not reload the module, thus the nested flag
    was actually false and the vmx flag was not exposed to the virtual
    machines.

--- puppet/modules/centos_cloud/manifests/compute.pp
+++ puppet/modules/centos_cloud/manifests/compute.pp
@@ -3,15 +3,12 @@ class centos_cloud::compute {
   include centos_cloud::compute::nova
   include centos_cloud::compute::neutron
 
-  file { '/etc/modprobe.d/kvm_intel.conf':
-    content => "options kvm_intel nested=1\n",
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+  include ::kmod
+  kmod::load { 'kvm_intel': }
+  kmod::option { 'kvm_intel':
+    option => 'nested',
+    value  => '1'
   }
 
-  exec { 'load vhost_net module':
-    command => '/usr/sbin/modprobe vhost_net',
-    creates => '/etc/modprobe.d/vhost.conf'
-  }
+  kmod::load { 'vhost_net': }
 }
